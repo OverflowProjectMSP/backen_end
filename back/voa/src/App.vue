1071d73954cc7bf295d6606c17f8f3dd839c427b
Surov Konstantin <151402518+Willyamdevon@users.noreply.github.com>
2024-04-14 00:22:33 +0300

    Add files via upload

---
+++ back/voa/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <User></User>
+  <S></S>
+  <Log></Log>
+  <Q></Q>
+  <Reg></Reg> -->
+  <Log></Log>
+  <User></User>
+</template>
+
+<script>
+
+import Log from './components/LogIn.vue'
+import User from './components/UserInfo.vue'
+import Reg from './components/Reg.vue'
+import Q from './components/QuetionComp.vue'
+// import S from './components/NewState.vue'
+import Q1 from './components/Quetions.vue'
+
+
+
+
+
+export default {
+  components:{
+    Log,
+    User,
+    Reg,
+    Q,
+    Q1
+  },
+};
+
+
+</script>
+
+<style>
+
+</style>
